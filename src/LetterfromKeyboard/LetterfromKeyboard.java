0cd994dbc07a33c0b0ff911029904b96cf607253
Lara8972 <svetlakova_07@mail.ru>
2025-07-26 11:48:31 +0700

    part 1 DZ lesson 8

---
+++ src/LetterfromKeyboard/LetterfromKeyboard.java
@@ -0,0 +1,39 @@
+package LetterfromKeyboard;
+import java.util.Scanner;
+
+
+public class LetterfromKeyboard {
+    public static void main(String[] args){
+        // Создан объект scan для чтения ввода из консоли
+        Scanner scan = new Scanner(System.in);
+
+        // создана строка, которая содержит все буквы клавиатуры английского алфавита попорядку
+        String keyboard = "qwertyuiopasdfghjklzxcvbnm";
+        //ввожу символ
+        System.out.println("Введите букву английского алфавита: ");
+
+        // считываю введеные символы, если введен не один символ,
+        // то забираю первый символ
+        char inputLetter = scan.nextLine().charAt(0);
+
+        // выполняю поиск индекса введенного символа из строки keyboard
+
+        int index = keyboard.indexOf(inputLetter);
+
+        // запускаю цикл если - то
+        if (index == -1) {
+            // Если введенный символ в консоль не найден в строке keyboard, то вывести
+            // сообщение
+            System.out.println("Введенный символ не является буквой английского алфавита");
+
+        } else {
+            // если введенная буква есть в строке keyboard, то
+            // находим индекс буквы слева из зацикленной строки keyboard
+            int leftIndex = (index - 1 + keyboard.length()) % keyboard.length();
+            // Выводим букву слева от введенной
+            System.out.println(keyboard.charAt(leftIndex));
+        }
+        scan.close();
+    }
+}
+
