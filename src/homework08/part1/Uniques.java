e96a1a221b9eba404b2acf15cb5b0b8f7347fb2b
Lara8972 <svetlakova_07@mail.ru>
2025-07-26 11:48:31 +0700

    create new branch homework08 and add ReadMe file

---
+++ src/homework08/part1/Uniques.java
@@ -0,0 +1,32 @@
+package homework08.part1;
+import java.util.ArrayList;
+
+public class Uniques {
+    public static <T> ArrayList<T> getUniques(ArrayList<T> list) { //Метод для извлечения уникальных элементов из списка
+        ArrayList<T> uniqueList = new ArrayList<>(); // новый список для хранения уникальных элементов
+
+        for (T element : list) { // прохожу по каждому элементу списка
+            // проверяю, не содержится ли элемент уже в списке уникальных
+            if (!uniqueList.contains(element)) {
+                // если элемент уникален, добавляю его в uniqueList
+                uniqueList.add(element);
+            }
+        }
+
+        return uniqueList; // список с уникальными элементами
+    }
+
+    public static void main(String[] args) {
+        // создала и заполнила ArrayList именами (элементами)
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Федор");
+        name.add("Степан");
+        name.add("Алиса");
+        name.add("Степан");
+        name.add("Федор");
+
+        ArrayList<String> uniqueName = getUniques(name);
+        System.out.println(uniqueName);  // вывод в консоль
+    }
+}
+
